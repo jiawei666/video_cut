f44bacd161246a1fbe40b69d23d4e45857250e94
yuanjiawei <957089263@qq.com>
2024-12-19 00:28:30 +0800

    update

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
-from moviepy import VideoFileClip, TextClip, CompositeVideoClip
+from moviepy import VideoFileClip, TextClip, CompositeVideoClip, ImageClip
 
 # Load file example.mp4 and keep only the subclip from 00:00:10 to 00:00:20
 # Reduce the audio volume to 80% of its original volume
 
 clip = (
-    VideoFileClip("/home/yuanjiawei/视频/特朗普.mp4")
-    .subclipped(2, 5)
+    VideoFileClip("特朗普.mp4")
+    .subclipped(5, 8)
     .with_volume_scaled(0.8)
 )
 
+image_clip = ImageClip("上海市·上海_2024年05月.png").resized(width=200).with_position('center').with_duration(3)  # For a picture
+
 # Generate a text clip. You can customize the font, color, etc.
 txt_clip = TextClip(
-    font="/usr/share/fonts/wps-office/DejaVuMathTeXGyre.ttf",
+    font="微软雅黑.ttf",
     text="Hello there!",
     font_size=70,
     color='white'
-).with_duration(10).with_position('center')
+).with_duration(3).with_position('center')
+
+
 
 # Overlay the text clip on the first video clip
-final_video = CompositeVideoClip([clip, txt_clip])
-final_video.write_videofile("result.mp4")
+final_video = CompositeVideoClip([clip, image_clip, txt_clip])
+final_video.write_videofile("result2.mp4")
